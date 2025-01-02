dff9f39a5b49aac24ebbfb6e006ced116c2a3047
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-02 09:36:49 +0500

    Reinitialize repository

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,119 @@
+// import axios from 'axios'
+// import { NewsDataType } from '@/types'
+// import Header from '@/components/Header'
+// import SearchBar from '@/components/SearchBar'
+// import React, { useEffect, useState } from 'react'
+// import BreakingNews from '@/components/BreakingNews'
+// import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+// import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+// type Props = {}
+
+// const Page = (props: Props) => {
+//   const { top: safeTop } = useSafeAreaInsets();
+//   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     getBreakingNews()
+//   }, []);
+
+//   const getBreakingNews = async () => {
+//     try {
+//       const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`
+//       const response = await axios.get(URL);
+
+//       if (response && response.data) {
+//         setBreakingNews(response.data.results);
+//         setIsLoading(false);
+//       }
+//     } catch (err: any) {
+//       console.log('Error Message: ', err.message);
+//     }
+//   }
+
+//   return (
+//     <View style={[styles.container, { paddingTop: safeTop }]}>
+//       <Header />
+//       <SearchBar />
+//       {isLoading ? (
+//         <ActivityIndicator size={'large'} />
+//       ) : (
+//         <BreakingNews newsList={breakingNews} />
+//       )}
+//     </View>
+//   )
+// }
+
+// export default Page;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// })
+
+import axios from "axios";
+import { NewsDataType } from "@/types";
+import Header from "@/components/Header";
+// import SearchBar from "@/components/searchBar";
+import React, { useEffect, useState } from "react";
+import BreakingNews from "@/components/BreakingNews";
+import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Categories from "@/components/Categories";
+import NewsList from "@/components/NewsList";
+
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const { top: safeTop } = useSafeAreaInsets();
+  const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getBreakingNews();
+  }, []);
+
+  const getBreakingNews = async () => {
+    try {
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`;
+      const response = await axios.get(URL);
+
+      if (response && response.data) {
+        setBreakingNews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (err: any) {
+      console.log("Error Message: ", err.message);
+    }
+  };
+
+  const onCatChanged = (Category: string)=>{
+    console.log('category', Category);
+    
+  }
+
+  return (
+    <ScrollView style={[styles.container, { paddingTop: safeTop }]}>
+      <Header />
+      {/* <SearchBar /> */}
+      {isLoading ? (
+        <ActivityIndicator size={"large"} />
+      ) : (
+        <BreakingNews newsList={breakingNews} />
+      )}
+      <Categories  onCategoryChanged={onCatChanged}/>
+      <NewsList newsList={breakingNews}/> 
+    </ScrollView>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ components/NewsList.tsx
@@ -0,0 +1,59 @@
+import { NewsDataType } from '@/types'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+
+type Props = {
+    newsList: Array<NewsDataType>
+}
+
+const NewsList = ({ newsList }: Props) => {
+    return (
+        <View style={styles.container} >
+            {newsList.map((item, index) => (
+                <View key={index} style={styles.itemContainer}>
+                    <Image source={{ uri: item.image_url }} style={styles.itemImage} />
+                    <View style={styles.itemInfo}>
+                        <Text style={styles.itemCategory}>{item.category}</Text>
+                        <Text style={styles.itemTitle}>{item.title}</Text>
+                    </View>
+                </View>
+            ))}
+        </View>
+    )
+}
+
+export default NewsList;
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 20,
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+        flex: 1,
+        gap: 10,
+    },
+    itemImage: {
+        width: 90,
+        height: 100,
+        borderRadius: 20,
+        marginRight: 10,
+
+    },
+    itemInfo:{
+        flex: 1,
+        gap: 10,
+        justifyContent: 'space-between',
+    },
+    itemCategory:{
+
+    },
+    itemTitle:{
+
+    },
+    
+
+
+})
