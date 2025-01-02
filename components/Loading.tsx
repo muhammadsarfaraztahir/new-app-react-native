0e65dcb00bdd60d43b30fee291d923fad6046003
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-02 10:05:41 +0500

    ......

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -15,10 +15,9 @@ export default Loading;
 
 const styles = StyleSheet.create({
     container: {
-        paddingHorizontal: 20,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center',
     },
     
 })
