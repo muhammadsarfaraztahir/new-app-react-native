ab5ff6e58cadd6c0e14638a2a7c5410a30f9ae3a
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-04 09:09:11 +0500

    ...

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,22 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import CheckBox from '@/components/CheckBox'
+import SearchBar from '@/components/SearchBar'
+import newsCategoryList from '@/constants/Categories'
+import { Colors } from '@/constants/Colors'
 import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = {}
 
-const Page = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Discover Screen</Text>
-    </View>
-  )
+const discover = (props: Props) => {
+    const { top: safeTop } = useSafeAreaInsets();
+    return (
+        <View style={[styles.container, {paddingTop: safeTop + 20}]} >
+           <SearchBar  withHorizontalPadding={false}/>
+           <Text style={styles.title}>Category</Text>
+           <View style={styles.listContainer}>
+            {newsCategoryList.map((item)=>(
+                <CheckBox />
+            ))}
+           </View>
+        </View>
+    )
 }
 
-export default Page
+export default discover;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+    },
+    title:{
+        fontSize: 10,
+            fontWeight: "600",
+            color: Colors.black,
+            marginBottom: 10,
+    },
+    listContainer:{
+        flexDirection:'row',
+        flexWrap: 'wrap',
+        gap: 16,
+        marginTop: 12,
+        marginBottom: 20,
+    },
 })
