bdc8fa8dd792b575b77d01f8be7cacb6ffc495a8
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-04 07:49:46 +0500

    ..

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,3 +1,50 @@
+// import React from 'react'
+// import { Colors } from '@/constants/Colors'
+// import { Ionicons } from '@expo/vector-icons'
+// import { StyleSheet, Text, View, } from 'react-native'
+// import { TextInput } from 'react-native-gesture-handler'
+
+
+// type Props = {}
+
+// const SearchBar = (props: Props) => {
+//     return (
+//         <View style={styles.container} >
+//             <View style={styles.searchBar}>
+//                 <Ionicons name='search-outline' size={20} color={Colors.lightGrey} />
+//                 <TextInput
+//                  placeholder='search' 
+//                  placeholderTextColor={Colors.lightGrey} 
+//                  style={styles.searchText}
+//                  autoCapitalize='none'
+//                  />
+//             </View>
+//         </View>
+//     )
+// }
+
+// export default SearchBar;
+
+// const styles = StyleSheet.create({
+//     container:{
+//         marginHorizontal: 20,
+//         marginBottom: 20,
+//     },
+//     searchBar:{
+//        backgroundColor: '#E4E4E4',
+//        paddingHorizontal: 10,
+//        paddingVertical: 12,
+//        borderRadius: 10,
+//        flexDirection: 'row',
+//        gap: 10,
+//     },
+//     searchText:{
+//        fontSize: 14,
+//        flex: 1,
+//        color: Colors.darkGrey, 
+//     },
+// })
+
 import React from 'react'
 import { Colors } from '@/constants/Colors'
 import { Ionicons } from '@expo/vector-icons'
@@ -5,11 +52,13 @@ import { StyleSheet, Text, View, } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
 
-type Props = {}
+type Props = {
+    withHorizontalPadding: boolean
+};
 
-const SearchBar = (props: Props) => {
+const SearchBar = ({withHorizontalPadding}: Props) => {
     return (
-        <View style={styles.container} >
+        <View style={[styles.container, withHorizontalPadding && {paddingHorizontal: 20}]} >
             <View style={styles.searchBar}>
                 <Ionicons name='search-outline' size={20} color={Colors.lightGrey} />
                 <TextInput
@@ -27,7 +76,7 @@ export default SearchBar;
 
 const styles = StyleSheet.create({
     container:{
-        marginHorizontal: 20,
+        // marginHorizontal: 20,
         marginBottom: 20,
     },
     searchBar:{
