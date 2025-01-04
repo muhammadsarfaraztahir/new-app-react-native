034b9f50df03058acdff055a5d0e7787eda4d936
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-04 09:24:55 +0500

    ...

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -14,7 +14,7 @@ const CheckBox = ({label, checked, onPress}: Props) => {
         <View style={styles.container} >
            <Text style={styles.label}>{label}</Text>
            <View>
-            <AntDesign name='checkcircle' size={14} color={Colors.tint}/>
+            <AntDesign name='checkcircle' size={14} color={Colors.tint} />
            </View>
         </View>
     )
