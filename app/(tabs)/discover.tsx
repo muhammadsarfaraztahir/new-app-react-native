2ccac82e16ae1f9c62b0f7d144cb8038a0e86f28
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-05 09:24:41 +0500

    ....

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -49,3 +49,72 @@
 //         marginBottom: 20,
 //     },
 // })
+
+import CheckBox from '@/components/CheckBox'
+import SearchBar from '@/components/SearchBar'
+import newsCategoryList from '@/constants/Categories'
+import { Colors } from '@/constants/Colors'
+import { useNewsCategories } from '@/hooks/useNewsCategories'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+type Props = {}
+
+const discover = (props: Props) => {
+    const { top: safeTop } = useSafeAreaInsets();
+    const {newsCategories, toggleNewsCategory}= useNewsCategories();
+    return (
+        <View style={[styles.container, {paddingTop: safeTop + 20}]} >
+           <SearchBar  withHorizontalPadding={false}/>
+           <Text style={styles.title}>Category</Text>
+           <View style={styles.listContainer}>
+            {newsCategories.map((item)=>(
+                <CheckBox
+                 key={item.id}
+                  label={item.title}
+                   checked={item.selected}
+                   onPress={() => {
+                    toggleNewsCategory(item.id)
+                   }}
+                   />
+                ))}
+                </View>
+           <Text style={styles.title}>Country</Text>
+           <View style={styles.listContainer}>
+            {newsCategories.map((item)=>(
+                <CheckBox
+                 key={item.id}
+                  label={item.title}
+                   checked={item.selected}
+                   onPress={() => {
+                    toggleNewsCategory(item.id)
+                   }}
+                   />
+            ))}
+           </View>
+        </View>
+    )
+}
+
+export default discover;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+    },
+    title:{
+        fontSize: 10,
+            fontWeight: "600",
+            color: Colors.black,
+            marginBottom: 10,
+    },
+    listContainer:{
+        flexDirection:'row',
+        flexWrap: 'wrap',
+        gap: 16,
+        marginTop: 12,
+        marginBottom: 20,
+    },
+})
