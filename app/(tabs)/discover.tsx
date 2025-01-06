a3fb94264c420cb7d9e7cd4a0e0f8d3291fb5141
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 09:52:46 +0500

    ...

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -56,58 +56,61 @@ import newsCategoryList from '@/constants/Categories'
 import { Colors } from '@/constants/Colors'
 import { useNewsCategories } from '@/hooks/useNewsCategories'
 import { useNewsCountries } from '@/hooks/useNewsCountry'
+import { Link } from 'expo-router'
 import React, { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = {}
 
 const discover = (props: Props) => {
     const { top: safeTop } = useSafeAreaInsets();
-    const {newsCategories, toggleNewsCategory}= useNewsCategories();
-    const {newsCountries, toggleNewsCountry}= useNewsCountries();
+    const { newsCategories, toggleNewsCategory } = useNewsCategories();
+    const { newsCountries, toggleNewsCountry } = useNewsCountries();
     const [searchQuery, setSearchQuery] = useState("");
     const [category, setCategory] = useState("");
     const [country, setCountry] = useState("");
 
     return (
-        <View style={[styles.container, {paddingTop: safeTop + 20}]} >
-           <SearchBar  
-           withHorizontalPadding={false}
-            setSearchQuery={setSearchQuery}
+        <View style={[styles.container, { paddingTop: safeTop + 20 }]} >
+            <SearchBar
+                withHorizontalPadding={false}
+                setSearchQuery={setSearchQuery}
             />
-           <Text style={styles.title}>Category</Text>
-           <View style={styles.listContainer}>
-            {newsCategories.map((item)=>(
-                <CheckBox
-                 key={item.id}
-                  label={item.title}
-                   checked={item.selected}
-                   onPress={() => {
-                    toggleNewsCategory(item.id);
-                    setCategory(item.slug);
-                   }}
-                   />
+            <Text style={styles.title}>Category</Text>
+            <View style={styles.listContainer}>
+                {newsCategories.map((item) => (
+                    <CheckBox
+                        key={item.id}
+                        label={item.title}
+                        checked={item.selected}
+                        onPress={() => {
+                            toggleNewsCategory(item.id);
+                            setCategory(item.slug);
+                        }}
+                    />
                 ))}
-                </View>
-           <Text style={styles.title}>Country</Text>
-           <View style={styles.listContainer}>
-            {newsCountries.map((item, index) => (
-                <CheckBox
-                 key={index}
-                  label={item.name}
-                   checked={item.selected}
-                   onPress={() => {
-                    toggleNewsCountry(index);
-                    setCountry(item.code);
-                   }}
-                   />
-            ))}
-           </View>
+            </View>
+            <Text style={styles.title}>Country</Text>
+            <View style={styles.listContainer}>
+                {newsCountries.map((item, index) => (
+                    <CheckBox
+                        key={index}
+                        label={item.name}
+                        checked={item.selected}
+                        onPress={() => {
+                            toggleNewsCountry(index);
+                            setCountry(item.code);
+                        }}
+                    />
+                ))}
+            </View>
 
-           <TouchableOpacity style={styles.searchBtn}>
-            <Text style={styles.searchBtnTxt}>Search</Text>
-           </TouchableOpacity>
+            <Link>
+                <TouchableOpacity style={styles.searchBtn}>
+                    <Text style={styles.searchBtnTxt}>Search</Text>
+                </TouchableOpacity>
+            </Link>
         </View>
     )
 }
@@ -119,29 +122,29 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 20,
     },
-    title:{
+    title: {
         fontSize: 10,
-            fontWeight: "600",
-            color: Colors.black,
-            marginBottom: 10,
+        fontWeight: "600",
+        color: Colors.black,
+        marginBottom: 10,
     },
-    listContainer:{
-        flexDirection:'row',
+    listContainer: {
+        flexDirection: 'row',
         flexWrap: 'wrap',
         gap: 16,
         marginTop: 12,
         marginBottom: 20,
     },
-    searchBtn:{
+    searchBtn: {
         backgroundColor: Colors.tint,
         alignItems: 'center',
         padding: 14,
         borderRadius: 10,
         marginVertical: 10,
     },
-    searchBtnTxt:{
-       color: Colors.white,
-       fontSize: 16,
-       fontWeight: '600',
+    searchBtnTxt: {
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: '600',
     },
 })
