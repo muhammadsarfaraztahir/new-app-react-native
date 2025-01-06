3e0c6633003b56c6b937868ac369fb5e3067757c
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 09:39:02 +0500

    ...

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -54,7 +54,7 @@ import { TextInput } from 'react-native-gesture-handler'
 
 type Props = {
     withHorizontalPadding: boolean
-    setSearchQuery
+    setSearchQuery :Function
 };
 
 const SearchBar = ({withHorizontalPadding}: Props) => {
