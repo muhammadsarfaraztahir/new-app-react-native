b78203043dacb1d475dda022e2299892012ce33b
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:24:50 +0500

    .

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -47,7 +47,7 @@ const Page = (props: Props) => {
         <>
         <Stack.Screen options={{
             headerLeft: () => (
-               <TouchableOpacity onPress={() => router.back}>
+               <TouchableOpacity onPress={() => router.back()}>
                 <Ionicons name='arrow-back' size={22}/>
                </TouchableOpacity> 
             )
