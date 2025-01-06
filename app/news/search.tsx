202ea9fc23476d8116aff0502d88eca024641b20
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:11:17 +0500

    ..

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -1,5 +1,5 @@
 import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 
 type Props = {}
