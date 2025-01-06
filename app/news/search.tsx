96befac8a98a19ae5338d9f73e66e96a021cd2c2
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:28:49 +0500

    ...

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -59,7 +59,7 @@ const Page = (props: Props) => {
            {isLoading ? (
             <Loading size={'large'}/> 
            ) : (
-            <FlatList data={}/> 
+            <FlatList data={news}/> 
            )}
         </View>
         </>
