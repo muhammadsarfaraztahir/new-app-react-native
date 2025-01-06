cb95b4714544d5910556ff838f902b10936abcd2
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:18:22 +0500

    ..

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -29,8 +29,8 @@ const Page = (props: Props) => {
           if( query){
             queryString = `&q=${query}`
           }
-          
-          const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}`;
+
+          const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}${}`;
           const response = await axios.get(URL);
     
           if (response && response.data) {
