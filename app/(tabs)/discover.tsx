b4781251a2c7e2f00a96777bed36b95eb50e7137
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 09:33:52 +0500

    ...

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -66,7 +66,7 @@ const discover = (props: Props) => {
     const { top: safeTop } = useSafeAreaInsets();
     const {newsCategories, toggleNewsCategory}= useNewsCategories();
     const {newsCountries, toggleNewsCountry}= useNewsCountries();
-    const [searchQuery, setSearchQuery]
+    const [searchQuery, setSearchQuery] = u
 
     return (
         <View style={[styles.container, {paddingTop: safeTop + 20}]} >
