2f9387be4b2f760c1779a03c8b91ab5223a5cd9e
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:12:43 +0500

    ...

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -18,6 +18,7 @@ const Page = (props: Props) => {
     const getNews = async () => {
         try {
           let categoryString = '';
+          let categoryString = '';
           if( category){
             categoryString = `&category=${category}`
           }
