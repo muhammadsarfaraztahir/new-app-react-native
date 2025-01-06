c9b2649ace7201a8eab5f0e736b2dda4845430b9
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 09:42:55 +0500

    ..

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -67,7 +67,7 @@ const SearchBar = ({withHorizontalPadding,setSearchQuery}: Props) => {
                  placeholderTextColor={Colors.lightGrey} 
                  style={styles.searchText}
                  autoCapitalize='none'
-                 onChangeText={query => }
+                 onChangeText={query => setSearchQuery}
                  />
             </View>
         </View>
