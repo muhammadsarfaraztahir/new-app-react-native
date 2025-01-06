499b6d163f582353404c4ab45409c4bf11e4411b
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:17:43 +0500

    ..

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -29,6 +29,7 @@ const Page = (props: Props) => {
           if( query){
             queryString = `&q=${query}`
           }
+          
           const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&removeduplicate=1&size=10${categoryString}`;
           const response = await axios.get(URL);
     
