49acb973aebab149a4c940f33ad118703bdfe89b
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-06 10:31:06 +0500

    ...

--- app/news/search.tsx
+++ app/news/search.tsx
@@ -59,7 +59,7 @@ const Page = (props: Props) => {
            {isLoading ? (
             <Loading size={'large'}/> 
            ) : (
-            <FlatList data={news} keyExtractor={(_, index) => `list_item${index}`} /> 
+            <FlatList data={news} keyExtractor={(_, index) => `list_item${index}`}  /> 
            )}
         </View>
         </>
