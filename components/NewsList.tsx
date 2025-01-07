80782d9cfcbf535149473826d40f6fc0742e613c
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 08:49:18 +0500

    ..

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -119,7 +119,7 @@ const NewsList = ({ newsList }: Props) => {
 
 export default NewsList;
 
-const NewsItem = ({item} : {item: NewsDataType} ) => {
+export const NewsItem = ({item} : {item: NewsDataType} ) => {
   return(
     <View style={styles.itemContainer}>
                 <Image
