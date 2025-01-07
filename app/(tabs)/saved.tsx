c846aac28e255684452d0d3c0b999ccd59cd5d1c
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 10:23:52 +0500

    ..

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -23,7 +23,7 @@ const Page = (props: Props) => {
         
         const response = await axios.get(`https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${query_string}`);
         const news = response.data.results;
-        setBookmarkNews();
+        setBookmarkNews(news);
       }
     })
   }
