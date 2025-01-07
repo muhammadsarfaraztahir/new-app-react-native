6d522d1f6f872fc120e44022e1110371a345c403
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 09:30:04 +0500

    .

--- app/news/[id].tsx
+++ app/news/[id].tsx
@@ -64,7 +64,7 @@ return (
             ),
             headerRight: () => (
                 <TouchableOpacity onPress={() => saveBookmark(news[0].article_id)}>
-                    <Ionicons name={bookmark ? "heart" :'heart-outline'} size={22}  color={bookmark ? 'r' }/>
+                    <Ionicons name={bookmark ? "heart" :'heart-outline'} size={22}  color={bookmark ? 'red' : Colors.black}/>
                 </TouchableOpacity>
             ),
             title: "",
