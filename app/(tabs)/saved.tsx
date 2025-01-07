0877531d50aeff742b505cc79c24bb69e315e427
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 10:30:38 +0500

    ..

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -37,7 +37,7 @@ const Page = (props: Props) => {
       headerShown: true,
     }} /> 
     <View style={styles.container}>
-      <Text>Saved Screen</Text>
+      {isLoading ? }
     </View>
     </>
   )
