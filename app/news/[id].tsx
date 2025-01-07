db84d47f18973aef38fe46fc98dd1ccbe17aae8e
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 09:49:08 +0500

    .....

--- app/news/[id].tsx
+++ app/news/[id].tsx
@@ -17,93 +17,94 @@ const NewsDetails = (props: Props) => {
     const [isLoading, setIsLoading] = useState(true);
     const [bookmark, setBookmark] = useState(false);
 
-    useEffect(() => (
+    useEffect(() => {
         getNews();
-      ))
-const getNews = async () => {
-    try {
-        const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${id}`;
-        const response = await axios.get(URL);
+    }, []);
 
-        if (response && response.data) {
-            setNews(response.data.results);
-            setIsLoading(false);
+    const getNews = async () => {
+        try {
+            const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&id=${id}`;
+            const response = await axios.get(URL);
+
+            if (response && response.data) {
+                setNews(response.data.results);
+                setIsLoading(false);
+            }
+        } catch (err: any) {
+            console.log("Error Message: ", err.message);
         }
-    } catch (err: any) {
-        console.log("Error Message: ", err.message);
-    }
-};
+    };
 
-const saveBookmark = async (newsId: string) => {
-    setBookmark(true);
-    await AsyncStorage.getItem("bookmark").then((token) => {
-        const res = JSON.parse(token);
-        if (res !== null) {
-            let data = res.find((value: string) => value === newsId);
-            if (data == null) {
-                res.push(newsId);
-                AsyncStorage.setItem("bookmark", JSON.stringify(res));
+    const saveBookmark = async (newsId: string) => {
+        setBookmark(true);
+        await AsyncStorage.getItem("bookmark").then((token) => {
+            const res = JSON.parse(token);
+            if (res !== null) {
+                let data = res.find((value: string) => value === newsId);
+                if (data == null) {
+                    res.push(newsId);
+                    AsyncStorage.setItem("bookmark", JSON.stringify(res));
+                    alert("News Saved");
+                }
+            } else {
+                let bookmark = [];
+                bookmark.push(newsId);
+                AsyncStorage.setItem("bookmark", JSON.stringify(bookmark))
                 alert("News Saved");
             }
-        } else {
-            let bookmark = [];
-            bookmark.push(newsId);
-            AsyncStorage.setItem("bookmark", JSON.stringify(bookmark))
-            alert("News Saved");
-        }
-    });
-};
+        });
+    };
 
-const removeBookmark = async (newsId: string) => {
-    setBookmark(false);
-    const bookmark = await AsyncStorage.getItem("bookmark").then((token) => {
-        const res = JSON.parse(token);
-        return res.filter((id: string) => id !== newsId);
-    });
-    await AsyncStorage.setItem('bookmark', JSON.stringify(bookmark));
-    alert("News unSaved!");
-}
+    const removeBookmark = async (newsId: string) => {
+        setBookmark(false);
+        const bookmark = await AsyncStorage.getItem("bookmark").then((token) => {
+            const res = JSON.parse(token);
+            return res.filter((id: string) => id !== newsId);
+        });
+        await AsyncStorage.setItem('bookmark', JSON.stringify(bookmark));
+        alert("News unSaved!");
+    }
 
-return (
-    <>
-        <Stack.Screen options={{
-            headerLeft: () => (
-                <TouchableOpacity onPress={() => router.back()}>
-                    <Ionicons name='arrow-back' size={22} />
-                </TouchableOpacity>
-            ),
-            headerRight: () => (
-                <TouchableOpacity onPress={() =>
-                    bookmark
-                        ? removeBookmark(news[0].article_id)
-                        : saveBookmark(news[0].article_id)}>
-                    <Ionicons name={bookmark ? "heart" : 'heart-outline'} size={22} color={bookmark ? 'red' : Colors.black} />
-                </TouchableOpacity>
-            ),
-            title: "",
-        }}
-        />
-        {isLoading ? (
-            <Loading size={'large'} />
-        ) : (
-            <ScrollView
-                contentContainerStyle={styles.contentContainer}
-                style={styles.container} >
-                <Text style={styles.title}>{news[0].title}</Text>
-                <View style={styles.newsInfoWrapper}>
-                    <Text style={styles.newsInfo}>{Moment(news[0].pubDate).format('MMMM DD, hh:mm a')}</Text>
-                    <Text style={styles.newsInfo}>{news[0].source_name}</Text>
-                </View>
-                <Image source={{ uri: news[0].image_url }} style={styles.newsImg} />
-                {news[0].content ? (
-                    <Text style={styles.newContent}>{news[0].content}</Text>
-                ) : (
-                    <Text style={styles.newContent}>{news[0].description}</Text>
-                )}
-            </ScrollView>
-        )}
-    </>
-)
+    return (
+        <>
+            <Stack.Screen options={{
+                headerLeft: () => (
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <Ionicons name='arrow-back' size={22} />
+                    </TouchableOpacity>
+                ),
+                headerRight: () => (
+                    <TouchableOpacity onPress={() =>
+                        bookmark
+                            ? removeBookmark(news[0].article_id)
+                            : saveBookmark(news[0].article_id)}>
+                        <Ionicons name={bookmark ? "heart" : 'heart-outline'} size={22} color={bookmark ? 'red' : Colors.black} />
+                    </TouchableOpacity>
+                ),
+                title: "",
+            }}
+            />
+            {isLoading ? (
+                <Loading size={'large'} />
+            ) : (
+                <ScrollView
+                    contentContainerStyle={styles.contentContainer}
+                    style={styles.container} >
+                    <Text style={styles.title}>{news[0].title}</Text>
+                    <View style={styles.newsInfoWrapper}>
+                        <Text style={styles.newsInfo}>{Moment(news[0].pubDate).format('MMMM DD, hh:mm a')}</Text>
+                        <Text style={styles.newsInfo}>{news[0].source_name}</Text>
+                    </View>
+                    <Image source={{ uri: news[0].image_url }} style={styles.newsImg} />
+                    {news[0].content ? (
+                        <Text style={styles.newContent}>{news[0].content}</Text>
+                    ) : (
+                        <Text style={styles.newContent}>{news[0].description}</Text>
+                    )}
+                </ScrollView>
+            )}
+        </>
+    )
 }
 
 export default NewsDetails;
