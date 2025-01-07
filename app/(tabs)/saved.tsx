39e604d86d694e08713ee2cc933393aa67f830f4
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 10:08:13 +0500

    ...

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -9,7 +9,7 @@ const Page = (props: Props) => {
   const [isLoading, setIsLoading ] =useState(true);
 
   const fetchBookmark = async() => {
-    await AsyncStorage.getItem('bookmark').then
+    await AsyncStorage.getItem('bookmark').then((token) => {})
   }
   return (
     <View style={styles.container}>
