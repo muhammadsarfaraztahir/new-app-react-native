06c1de551abad5992deb6d7fe13d1fcddf3fe8aa
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-07 09:15:45 +0500

    ...

--- app/news/[id].tsx
+++ app/news/[id].tsx
@@ -45,6 +45,8 @@ const NewsDetails = (props: Props) => {
                 AsyncStorage.setItem("bookmark", JSON.stringify(res));
                 alert("News Saved");
             }
+           } else {
+            
            }
         })
       }
